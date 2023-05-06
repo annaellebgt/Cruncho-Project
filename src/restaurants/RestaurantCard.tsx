43acdd431eed2c08632b52dd73dd88a73f0db681
Annaëlle <annaelle.baiget@student-cs.fr>
2023-05-06 20:42:55 +0200

    add icon + default image

--- src/restaurants/RestaurantCard.tsx
+++ src/restaurants/RestaurantCard.tsx
@@ -10,14 +10,17 @@ function RestaurantCard({ restaurant, currentPosition }: restaurantCardProps) {
   type State = {
     distance: number | null;
     urlPhoto: string;
+    photoAttribution: string;
   };
   type Action =
     | { type: "SET_DISTANCE"; payload: number }
-    | { type: "SET_URL_PHOTO"; payload: string };
+    | { type: "SET_URL_PHOTO"; payload: string }
+    | { type: "SET_PHOTO_ATTRIBUTION"; payload: string };
 
   const initialState: State = {
     distance: null,
     urlPhoto: "",
+    photoAttribution: "",
   };
 
   function reducer(state: State, action: Action): State {
@@ -31,6 +34,11 @@ function RestaurantCard({ restaurant, currentPosition }: restaurantCardProps) {
         return {
           ...state,
           urlPhoto: action.payload,
+        };
+      case "SET_PHOTO_ATTRIBUTION":
+        return {
+          ...state,
+          photoAttribution: action.payload,
         };
       default:
         return state;
@@ -57,24 +65,42 @@ function RestaurantCard({ restaurant, currentPosition }: restaurantCardProps) {
     if (restaurant.photos && restaurant.photos.length > 0) {
       photo = restaurant.photos[0];
       const urlphoto = photo.getUrl({ maxWidth: 350, maxHeight: 250 });
+      const photoAttribution = photo.html_attributions[0];
       dispatch({ type: "SET_URL_PHOTO", payload: urlphoto });
+      dispatch({ type: "SET_PHOTO_ATTRIBUTION", payload: photoAttribution });
     }
   }, [restaurant.geometry, currentPosition, restaurant.photos]);
 
   return (
     <Card className="my-3">
       <Card.Body>
-        {state.urlPhoto && (
-          <Card.Header>
-            <img src={state.urlPhoto} alt={restaurant.name}></img>
-          </Card.Header>
-        )}
+        <Card.Header>
+          <img
+            src={
+              state.urlPhoto || process.env.PUBLIC_URL + "/assets/default.png"
+            }
+            alt="Restaurant"
+            style={{ maxWidth: 350, maxHeight: 250 }}
+          />
+        </Card.Header>
 
         <Card.Title>{restaurant.name}</Card.Title>
+
         <Card.Text>Rating: {restaurant.rating}</Card.Text>
         <Card.Text>Address : {restaurant.vicinity}</Card.Text>
-
         <Card.Text>Distance: {state.distance}</Card.Text>
+
+        <Card.Text>
+          Icon : <img src={restaurant.icon} alt="..."></img>
+        </Card.Text>
+        <Card.Text>Url : {restaurant.url}</Card.Text>
+        <Card.Text>
+          User Rating Total : {restaurant.user_ratings_total}
+        </Card.Text>
+
+        {state.photoAttribution && (
+          <div dangerouslySetInnerHTML={{ __html: state.photoAttribution }} />
+        )}
 
         <div>
           {restaurant.types?.map((type, index) => (
